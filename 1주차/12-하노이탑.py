63551590ceb6e25ce276d85027c65b36b6ec1824
Alisser <gnlrjswjd@naver.com>
2024-07-05 17:39:08 +0900

    Create 12-하노이탑.py

---
+++ 1주차/12-하노이탑.py
@@ -0,0 +1,12 @@
+def hanoi(n, a, b, c):
+    if n == 1:
+        print(a, c)
+    else:
+        hanoi(n-1, a, c, b)
+        print(a, c)
+        hanoi(n-1, b, a, c)
+N = int(input())
+sum = 2 ** N -1
+print(sum)
+if N <=20:
+    hanoi(N, 1, 2, 3)
