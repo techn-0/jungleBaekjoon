8dd7c4782be2b6cebfbd11d19d2fe545c59b34bd
Techn0 <68315853+techn-0@users.noreply.github.com>
2024-07-10 10:26:12 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,10 +1,6 @@
-def iostr():
-    m, s = input().split()
-    for i in range(0, len(s)):
-        for j in range(int(m)):
-            print(s[i], end='')
-    print()
-
 n = int(input())
 for i in range(n):
-    iostr()
+    P, S = input().split()
+    for i in S:
+        print(i * int(P), end='')
+    print()
