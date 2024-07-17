a309257e8d309fd3588ec6392bfa6c2b71662487
Techn0 <68315853+techn-0@users.noreply.github.com>
2024-07-17 22:14:31 +0900

    Update 트리 순회.py

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,39 +1,46 @@
 import sys
 input = sys.stdin.readline
-class nod:
-    def __init__(self,item, left, right):
+
+class node:
+    def __init__(self, item, L, R):
         self.item = item
-        self.left = left
-        self.right = right
-def pre(nod):
-    print(nod.item, end = '')
-    if nod.left !='.':
-        pre(tree[nod.left])
-    if nod.right !='.':
-        pre(tree[nod.right])
-def mid(nod):
-    if nod.left !='.':
-        mid(tree[nod.left])
-    print(nod.item, end = '')
-    if nod.right !='.':
-        mid(tree[nod.right])
-def post(nod):
-    if nod.left !='.':
-        post(tree[nod.left])
-    
-    if nod.right !='.':
-        post(tree[nod.right])
-    print(nod.item, end = '')
-n = int(input())
+        self.L = L
+        self.R = R
+
+def pre(node):
+    print(node.item, end = '')
+    if node.L != '.':
+        pre(tree[node.L])
+    if node.R != '.':
+        pre(tree[node.R])
+
+def mid(node):
+    if node.L != '.':
+        mid(tree[node.L])
+    print(node.item, end = '')
+    if node.R != '.':
+        mid(tree[node.R])
+
+def post(node):
+    if node.L != '.':
+        post(tree[node.L])
+    if node.R != '.':
+        post(tree[node.R])
+    print(node.item, end = '')
+
+N = int(input())
 arr = []
-for i in range(n):
+
+for i in range(N):
     arr.append(input().split())
+
 tree = {}
-for item, left, right in arr:
-    tree[item] = nod(item, left, right)
+
+for item, L, R in arr:
+    tree[item] = node(item, L, R)
+
 pre(tree['A'])
 print()
 mid(tree['A'])
 print()
 post(tree['A'])
-print()
