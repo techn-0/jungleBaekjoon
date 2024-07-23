8f61967c905c3e9aefd911af0ea892d4c41b6833
Techn0 <68315853+techn-0@users.noreply.github.com>
2024-07-23 21:01:31 +0900

    [Silver I] Title: 신입 사원, Time: 5376 ms, Memory: 215920 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,14 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    arr = []
+    for _ in range(N):
+        arr.append(list(map(int, input().split())))
+    asc = sorted(arr)
+    top = 0
+    result = 1   # 첫 번째 사원 채용
+    for i in range(1, len(asc)):
+        if asc[i][1] < asc[top][1]: # 현재 사원보다 면접 점수가 낮은 경우
+            top = i
+            result += 1
+    print(result)
