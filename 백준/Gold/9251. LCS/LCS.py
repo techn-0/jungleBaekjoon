03112ce64ea5cb3608f9254cac38a13ed7f189a7
Techn0 <68315853+techn-0@users.noreply.github.com>
2024-07-24 20:17:42 +0900

    [Gold V] Title: LCS, Time: 156 ms, Memory: 118256 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,10 +1,11 @@
 s1 = list(input())
 s2 = list(input())
-arr = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
 for i in range(1, len(s1) + 1):
     for j in range(1, len(s2) + 1):
         if s1[i - 1] == s2[j - 1]:
-            arr[i][j] = arr[i-1][j-1] + 1
+            dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            arr[i][j] = max(arr[i - 1][j], arr[i][j - 1])
-print(max(map(max, arr)))
+            dp[i][j] = max(dp[i - 1][j],dp[i][j - 1])
+print(max(map(max,dp)))
