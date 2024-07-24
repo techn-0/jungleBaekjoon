14c7c2561e11994284f7d0832fa78a0e34ac73e8
Techn0 <68315853+techn-0@users.noreply.github.com>
2024-07-24 19:54:05 +0900

    [Silver III] Title: 01타일, Time: 168 ms, Memory: 184624 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,10 +1,7 @@
-N = int(input())
-
+n = int(input())
 arr = []
 arr.append(0)
 arr.append(1)
-
-for i in range(2, N + 2):
-    arr.append((arr[i - 1] + arr[i - 2]) % 15746)
-
-print(arr[N + 1])
+for i in range(2, n + 2):
+    arr.append((arr[i-1] + arr[i - 2]) % 15746)
+print(arr[n + 1])
