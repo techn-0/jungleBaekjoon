bdaffc145ef69d2c7dc90e7c100a63bee7557268
Alisser <gnlrjswjd@naver.com>
2024-07-25 11:30:30 +0900

    1

--- 3주차 시험/2- 1890.py
+++ 3주차 시험/2- 1890.py
@@ -1,29 +1,26 @@
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
 N = int(input())
+
 gameboard = []
 for _ in range(N):
     gameboard.append(list(map(int, input().split())))
 
-
 dp = [[-1] * N for _ in range(N)]
-
-mov = [(1,0),(0,1)]
+mov = [(1, 0), (0, 1)]
 
 def DFS(x, y):
-    if x == N-1 and y == N-1:
+    if x == N-1 and y == N-1: # 목표 지점인가?
         return 1
-    if dp[x][y] != -1:
+    if dp[x][y] != -1: # 이미 경로가 있다면
         return dp[x][y]
-    else:
-        dp[x][y] = 0
-        for i in mov:
-            nx = x + i[0] * gameboard[x][y]
-            ny = y + i[1] * gameboard[x][y]
-            if 0 <= nx < N and 0 <= ny < N:
-                dp[x][y] += DFS(nx,ny)
+    
+    dp[x][y] = 0
+    for dx, dy in mov:
+        nx = x + dx * gameboard[x][y]
+        ny = y + dy * gameboard[x][y]
+        if 0 <= nx < N and 0 <= ny < N:# 범위를 벗어나지 않았다면
+            dp[x][y] += DFS(nx, ny)
     return dp[x][y]
-
-print(DFS(0,0))
+print(DFS(0, 0))
--- 3주차 시험/3 - 1379.py
+++ 3주차 시험/3 - 1379.py
@@ -6,13 +6,13 @@ for i in range(N):
 
 arr.sort(key=lambda x: (x[2], x[1]))
 
-end = arr[0][2] # 첫 번째 회의를 선택, 종료 시간 end 저장
+end = arr[0][2]
 index.append(arr[0][0])
 count = 1
 for i in range(1, N):
-    if end <= arr[i][1]: # 현재 회의의 시작 시간이 이전 회의의 종료 시간 이후일 경우
+    if end <= arr[i][1]:
         count += 1
-        end = arr[i][2] # 현재 회의의 종료 시간으로 업데이트
+        end = arr[i][2]
         index.append(arr[i][0])
 print(count)
 print(index)
