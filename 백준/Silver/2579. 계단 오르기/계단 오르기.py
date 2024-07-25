78ed1561724691d1632f0b855503e330fb657cf5
Techn0 <68315853+techn-0@users.noreply.github.com>
2024-07-25 10:41:12 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,14 @@
+N = int(input())
+str = [0] * (N + 3)
+arr = [0] * (N + 3)
+
+for i in range(N):
+    str[i] = int(input())
+
+arr[0] = str[0]
+arr[1] = str[0] + str[1]
+arr[2] = max(str[0] + str[2], str[1] + str[2])
+
+for i in range(3, N):
+    arr[i] = max(arr[i - 3] + str[i - 1] + str[i], arr[i - 2] + str[i])
+print(arr[N-1])
