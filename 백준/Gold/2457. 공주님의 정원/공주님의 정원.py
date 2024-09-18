2150812aa9b96c452b875730bc662ab0d23136d5
Techn0 <68315853+techn-0@users.noreply.github.com>
2024-09-18 14:08:19 +0900

    [Gold III] Title: 공주님의 정원, Time: 396 ms, Memory: 122432 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -1,32 +1,30 @@
 num = int(input())
 
-f = []
-for i in range(num):
-    flower = list(map(int, input(). split()))
-    f.append(flower)
+f = [list(map(int, input().split())) for _ in range(num)]
 f.sort()
 
 total = 0
-end_of_world = (3, 1)
+end_of_world = (3, 1)  # 3월 1일부터 시작
+last_flower_end = (0, 0)
 
 i = 0
-while i < num:
-    s_month, s_day, e_month, e_day = f[i]
-    if (s_month, s_day) <= end_of_world < (e_month, e_day):
-        temp_end = (e_month, e_day)
-        while i < num - 1:
-            next_s_month, next_s_day, next_e_month, next_e_day = f[i+1]
-            if end_of_world < (next_s_month, next_s_day):
-                break
-            if temp_end < (next_e_month, next_e_day):
-                temp_end = (next_e_month, next_e_day)
-            i += 1
+while i < num and end_of_world <= (11, 30):
+    max_end = (0, 0)
+    while i < num:
+        s_month, s_day, e_month, e_day = f[i]
+        if (s_month, s_day) > end_of_world:  # 꽃이 현재 범위 밖이면 종료
+            break
+        if max_end < (e_month, e_day):
+            max_end = (e_month, e_day)  # 더 긴 꽃의 종료일 갱신
+        i += 1
 
-        total += 1
-        end_of_world = temp_end
+    if max_end == (0, 0):  # 더 이상 선택할 꽃이 없으면 실패
+        break
 
-        if (11, 30) < end_of_world:
-            print(total)
-            exit(0)
-    i += 1
-print(0)
+    end_of_world = max_end  # 현재 범위 확장
+    total += 1  # 사용한 꽃의 수 증가
+
+if end_of_world > (11, 30):
+    print(total)
+else:
+    print(0)
