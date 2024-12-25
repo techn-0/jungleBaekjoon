52bc399fe138010e04ba2699bdfaf549ac26f11e
Alisser <gnlrjswjd@naver.com>
2024-12-25 12:37:54 +0900

    Update 64061.py

--- 프로그래머스/크레인 인형뽑기 게임/64061.py
+++ 프로그래머스/크레인 인형뽑기 게임/64061.py
@@ -5,5 +5,15 @@ def solution(board, moves):
 
     for i in moves:
         for j in range(len(board)):
-            if
+            if board[j][i - 1] == 0:
+                continue
+            else:
+                basket.append(board[j][i-1])
+                board[j][i-1] = 0 #뽑았으니 0으로
+                if len(basket) > 1:
+                    if basket[-1] == basket[-2]: #맨위에 2개 비교
+                        basket.pop() #두개 터지니까 두번
+                        basket.pop() # 어 왜 두번 써지지 어 왜 두번 써지지
+                        answer += 2
+            break
     return answer
