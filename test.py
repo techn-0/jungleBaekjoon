eccd4132d1e84ae098af66b5b8341069ff18dba4
Alisser <gnlrjswjd@naver.com>
2024-12-30 17:15:33 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,28 @@
-n, k = map(int, input().split())
+def solution(id_list, report, k):
+    result = [0] * len(id_list) # 일단 result 를 유저 수만큼 크기를가진 리스트로 선언해줌
 
-thing = [[0,0]]
-d = [[0]*(k+1) for _ in range(n+1)]
+    report = list(set(report)) # 중복 신고 제거
 
-# 물건 리스트
-for i in range(n):
-    thing.append(list(map(int, input().split())))
+    reports = {}
+    # 신고 내역 딕셔너리를만들건데 신고당한유저가 키값이고 신고한 유저를 벨류로 잡으면 정지된 유저를 신고한 사람에게 문자를 보내기 편할거야
 
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        w = thing[i][0]
-        v = thing[i][1]
+    for i in report:
+        Rer, Red = i.split()
+        if Red not in reports: # 리폿당한 유저가 딕셔너리에 없음 빈 리스트 추가
+            reports[Red] = []
+        reports[Red].append(Rer) # 신고당한 사람 딕셔너리에 신고한 사람을 벨류로 넣어줌
 
-        if j < w:   # 물건무계가 더 커서 안들어간다면
-            d[i][j] = d[i-1][j]
-        else:
-            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v)
-            # 현재 넣을 물건의 크기만큼 빼고 현재 물건을 넣는이유
-            # 이미 물건을 넣은 상태에서 새 물건이 들어갈 공간이 확보되는지 확인하기 위함
+    die = [] # 처형할 유저 리스트
 
-print(d[n][k])
+    for user, reN in reports.items(): # k번 이상 신고당한 유저 선별
+        if len(reN) >= k:
+            die.append(user)
+
+    mail = [0] * len(id_list) # 유저별 알림 발송 횟수 리스트 맹그러둠
+    
+    for dieU in die:
+        for Rer in reports[dieU]:
+            mail[id_list.index(Rer)] += 1  # 신고자를 mail 리스트에 반영
+
+    answer = mail  # 최종 메일 발송 횟수 리스트를 answer에 담기
+    return answer
