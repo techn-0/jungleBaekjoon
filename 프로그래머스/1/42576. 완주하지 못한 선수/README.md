47b70ade963d6dfa301fa45ab65f2ba914a7309a
JEONG HWI GEON <68315853+techn-0@users.noreply.github.com>
2025-12-11 13:56:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.80 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 33.1 MB, 시간: 44.31 ms
+메모리: 33 MB, 시간: 42.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 03일 20:17:14
+2025년 12월 11일 13:56:20
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,22 +1,16 @@
 def solution(participant, completion):
-    answer = ''
-    
-    cnt = {}
+    dic = {}
     
     for i in participant:
-        if i in cnt:
-            cnt[i] += 1
+        if i in dic:
+            dic[i] += 1
         else:
-            cnt[i] = 1
+            dic[i] = 1
+    # print(dic)
     for i in completion:
-        cnt[i] -= 1
-    
-    for k, v in cnt.items():
-        if v != 0:
-            return k
-    return answer
-
-# participant: 참여선수 이름
-# completion: 완주 선수 이름
-# answer: 완주 못한 선수 이름
-# 동명이인 처리해야함
+        dic[i] -= 1
+    for a, b, in dic.items():
+        # print(a, b)
+        if b != 0:
+            return a
+        
