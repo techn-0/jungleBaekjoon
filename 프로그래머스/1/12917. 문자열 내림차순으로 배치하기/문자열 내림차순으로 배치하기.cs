5a5d524fd1bf0db30c5f7c3d1c18b9490b568434
JEONG HWI GEON <68315853+techn-0@users.noreply.github.com>
2026-01-13 14:17:31 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 2.06 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cs
@@ -0,0 +1,14 @@
+using System;
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+        
+        char[] chars = s.ToCharArray();
+        
+        Array.Sort(chars);
+        Array.Reverse(chars);
+        
+        answer = new string(chars);
+        return answer;
+    }
+}
