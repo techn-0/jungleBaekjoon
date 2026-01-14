b4a69816ae5010e41032432f8477454f052b156b
JEONG HWI GEON <68315853+techn-0@users.noreply.github.com>
2026-01-14 16:32:02 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.31 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        int gcd = GCD(n, m);
+        answer[0] = gcd;
+        
+        answer[1] = (n*m) / gcd;
+        
+        
+        return answer;
+    }
+        
+    private int GCD(int a, int b)
+    {
+        if(b == 0)
+            return a;
+        else
+            return GCD(b, a%b);
+    }
+}
