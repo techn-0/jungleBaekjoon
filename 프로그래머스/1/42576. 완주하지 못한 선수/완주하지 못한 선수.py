155134cb9b7b08608bc216aa06c2877348138b47
JEONG HWI GEON <68315853+techn-0@users.noreply.github.com>
2026-04-06 16:55:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.70 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,15 +2,12 @@ def solution(participant, completion):
     dic = {}
     
     for i in participant:
-        if i in dic:
-            dic[i] += 1
-        else:
+        if i not in dic:
             dic[i] = 1
-    # print(dic)
-    for i in completion:
-        dic[i] -= 1
-    for a, b, in dic.items():
-        # print(a, b)
+        else:
+            dic[i] += 1
+    for j in completion:
+        dic[j] -= 1
+    for a, b in dic.items():
         if b != 0:
             return a
-        
