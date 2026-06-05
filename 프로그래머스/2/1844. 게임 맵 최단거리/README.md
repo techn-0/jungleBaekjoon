f8e9f794e375e3205aa7de73aec3b9ff6ceefc33
JEONG HWI GEON <68315853+techn-0@users.noreply.github.com>
2026-06-05 23:38:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 1.59 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 30.9 MB, 시간: 1.27 ms
+메모리: 30.6 MB, 시간: 1.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 26일 15:40:25
+2026년 06월 05일 23:38:18
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
@@ -3,36 +3,34 @@ using System.Collections.Generic;
 
 class Solution {
     public int solution(int[,] maps) {
-        int rows = maps.GetLength(0);
-        int cols = maps.GetLength(1);
-        
-        Queue<(int x, int y)> que = new Queue<(int x, int y)>();
-        int[,] disit = new int[rows, cols];
-        
-        que.Enqueue((0,0));
-        disit[0, 0] = 1;
+        int row = maps.GetLength(0);
+        int col = maps.GetLength(1);
+        int[,] dis = new int[row, col];
+        Queue<(int x, int y)> que = new Queue<(int, int)>();
         
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
+        dis[0,0] = 1;
+        que.Enqueue((0,0));
         
-        while(que.Count > 0){
-            var (x,y) = que.Dequeue();
+        while(que.Count>0){
+            var(x,y) = que.Dequeue();
             
-            for(int i = 0; i<4; i++)
-            {
-                int nx = x+dx[i];
-                int ny = y+dy[i];
+            for(int i = 0; i<4; i++){
+                int nx = x +dx[i];
+                int ny = y +dy[i];
+                //맵 밖인가?
+                if(nx < 0 || nx>= row || ny < 0 || ny >= col) continue;
+                // 벽인가?
+                if(maps[nx, ny] == 0) continue;
+                // 이미 방문 했나?
+                if(dis[nx, ny] != 0) continue;
                 
-                if(nx<0 || ny<0 || nx>=rows || ny>=cols)  continue;
-                if(disit[nx, ny] != 0)    continue;
-                if(maps[nx, ny] == 0)   continue;
-                
-                disit[nx, ny] = disit[x,y] + 1;
+                dis[nx, ny] = dis[x,y] + 1;
                 que.Enqueue((nx, ny));
-                
             }
         }
-        int goal = disit[rows - 1, cols - 1];
-        return goal == 0? -1:goal;
+        int answer = dis[row-1, col-1];
+        return answer == 0 ? -1 : answer;
     }
 }
